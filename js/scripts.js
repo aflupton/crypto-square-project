5fb1b7febddf2063d69810ed4f39655c0c43abb4
andrew lupton and Brian Henry <brian@>
2018-03-29 16:09:08 -0700

    Add some extra stuff

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,40 @@
   //   alert("Please enter a sentence or string of letters.")
 
 var cypher = "";
-var row3 = [];
-var row1 = [];
-var row2 = [];
+var columns = [];
+var tempCypher = [];
+
+var finalStr = "";
+
+
+// };  console.log(columns);
+//
+// var array = [[], [], [], [], []];
+//
+// for (var i in a)
+// {
+//    console.log("row " + i);
+//    for (var j in a[i])
+//      {
+//       console.log(" " + a[i][j]);
+//      }
+var even = false
+// remove spaces from string
+  cypher = cypher.replace(/\s/g, "");
+// split string into array
+  tempCypher = cypher.split("");
+
+// var tempCypher = cypher.split("");
+console.log(cypher.length);
+//determine size of grid
+var rows = function(cypher) {
+  var squareRoot = (Math.sqrt(cypher.length);
+  if (squareRoot % 1 ==0) {
+    even = true
+  } else {
+    return (Math.floor(Math.sqrt(cypher.length)));
+  }
+}
 
 
 // UI logic
@@ -14,15 +45,6 @@ $(document).ready(function() {
   $("form#encrypt").submit(function(event) {
     event.preventDefault();
     cypher = $("input#encrypt").val().toLowerCase();
-
-    // function encrypt(){
-      cypher = cypher.replace(/\s/g, "");
-      cypher = cypher.split("");
-      // var tempCypher = cypher.split("");
-    console.log(cypher.length);
-
-    // };
-
 
 
     $("#result").show();
